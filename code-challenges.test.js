1cb82baafd7be15b6dd02ede12c79e048a7a96c5
Sean <swbriski@gmail.com>
2023-10-23 00:09:08 -0700

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -20,9 +20,40 @@ const fibonacciLength1 = 6
 const fibonacciLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+describe ("fibonacciSequence", () => {
+  it("Takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.", () => {
+    expect(fibonacciSequence(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibonacciSequence(fibonacciLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    expect(fibonacciSequence(2)).toEqual("Error, the requested value must be a number greater than 2")
+  })
+})
+
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+  // fibonacci sequence: beginning with the third number in the sequence, each number is equivalent to the sum of the previous two numbers
+  // create a function called fibonacciSequence
+    // input: number; replaces parameter named "sequenceLength"; represents length of desired array
+      // output: array with the desired number of values from the fibonacci sequence
+    // Create an array called fibonacciBase that is equivalent to [1, 1]; these are the initial two values of the fibonnacci sequence and are required to make the sequence work
+    // Create a decision tree
+      // if input is not a number OR if the input is 2 or less
+        // return an error
+      // else, use a for loop
+        // .push a value into fibonacciBase that is equivalent to the sum of the previous two numbers
+        // return new fibonacciBase
+
+const fibonacciSequence = (sequenceLength) => {
+  var fibonacciBase = [1, 1]
+  if (typeof sequenceLength !== 'number' || sequenceLength <=2) {
+    return "Error, the requested value must be a number greater than 2"
+  } else {
+    for (i = 0; i < sequenceLength-2; i++) {
+      fibonacciBase.push(fibonacciBase[i]+fibonacciBase[i+1])
+    }
+    return fibonacciBase
+  }
+}
 
 // --------------------2) For the following prompt, use ChatGPT or an equivalent AI tool to help you solve the problem. The prompt will require the use of a JavaScript method called Object.values()
 // Prompt: Create a function that takes in an object and returns an array of the object's values sorted from least to greatest.
@@ -51,6 +82,23 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
+describe ("sortObjectValues", () => {
+  it("takes in an object and returns an array of the object's values sorted from least to greatest", () => {
+      expect(sortObjectValues(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+      expect(sortObjectValues(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
+
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+  // Create a function called sortObjectValues
+    // input: object
+      // output: array with the object's values sorted from least to greatest
+    // create a variable called objectValues that uses Object.values(); this will create an array containing each of that object's values
+      // use .sort() to arrange values in ascending order
+
+const sortObjectValues = (object) => {
+  var objectValues = Object.values(object)
+  return objectValues.sort((a,b) => a - b)
+}
